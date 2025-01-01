ed048a17b5ddd779f138e01a7ab7953e13efd0a6
vishnu-mukati <mukativishnu1232@gmaila.com>
2025-01-01 14:24:00 +0530

    incrementing and decrementing action by 2

--- react-demo.js
+++ react-demo.js
@@ -1,20 +1,27 @@
 const redux = require('redux');
 
-const counterReducer = (state = {counter : 0},action) =>{
-    return {
-        counter : state.counter - 1
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter + 2
+        }
     }
+    if (action.type === 'decrement') {
+        return {
+            counter: state.counter - 2
+        }
+    }
+    return state;
 }
 
 const store = redux.createStore(counterReducer);
 
 
-const counterSubscribe = () =>{
+const counterSubscribe = () => {
     const lateststate = store.getState();
     console.log(lateststate);
 }
 
 store.subscribe(counterSubscribe);
-for(let i=0;i<5;i++){
-    store.dispatch( {type : 'decrement'});
-}
+    store.dispatch({ type: 'increment' });
+    store.dispatch({ type: 'decrement' });
